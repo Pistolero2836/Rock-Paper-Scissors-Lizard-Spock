658ecca9c4d32e79278230181cb2b4cc184f3602
Nathan Falco Ryan De Gaetano <112250123+Pistolero2836@users.noreply.github.com>
2025-03-22 17:18:21 +0100

    Create Rock-Paper-Scissors-Lizard-Spock.py
    Okay, you have played Rock, Paper, Scissors, but have you heard of Rock, Paper, Scissors, Lizard, Spock? It's a fun variation of the classic game brought to popularity with a TV show called The Big Bang Theory.
    
    The rules are very similar to the ones from the classic “Rock Paper Scissors” but with two new options, “Lizard” and “Spock”:
    
    Scissors cut Paper.
    Paper covers Rock.
    Rock crushes Lizard.
    Lizard poisons Spock.
    Spock smashes Scissors.
    Scissors beat Lizard.
    Lizard eats Paper.
    Paper disproves Spock.
    Spock vaporizes Rock.
    Rock breaks Scissors.

---
+++ Rock-Paper-Scissors-Lizard-Spock.py
@@ -0,0 +1,81 @@
+import random
+
+
+rock = '✊'
+paper = '✋'
+scissor = '✌️'
+spock = '🖖'
+lizard = '🦎'
+
+
+print('================================\nRock Paper Scissors Lizard Spock\n================================')
+print(f'1) {rock}')
+print(f'2) {paper}')
+print(f'3) {scissor}')
+print(f'4) {lizard}')
+print(f'5) {spock}')
+
+player = int(input('Pick a number: '))
+computer = random.randint(1, 3)
+
+print('')
+
+print(f'Your choose: {player}')
+print(f'CPU choose: {computer}')
+
+if player == 1 and computer == 3 :
+    print('The player won!🙋')
+elif player == 3 and computer == 2 :
+    print('The Player won!🙋')
+elif player == 2 and computer == 1 :
+    print('The player won!🙋')
+elif player == 1 and computer == 4 :
+    print('The player won!🙋')
+elif player == 4 and computer == 5 :
+    print('The player won!🙋')
+elif player == 5 and computer == 3 :
+    print('The player won!🙋')
+elif player == 3 and computer == 4 :
+    print('The player won!🙋')
+elif player == 4 and computer == 2 :
+    print('The player won!🙋')
+elif player == 2 and computer == 5 :
+    print('The player won!🙋')
+elif player == 5 and computer == 1 :
+    print('The player won!🙋')
+elif player == 1 and computer == 3 :
+    print('The player won!🙋')
+elif computer == 1 and player == 3 :
+    print('The CPU won!💻')
+elif computer == 3 and player == 2 :
+    print('The CPU won!💻')
+elif computer == 2 and player == 1 :
+    print('The CPU won!💻')
+elif computer == 1 and player == 4 :
+    print('The CPU won!💻')
+elif computer == 4 and player == 5 :
+    print('The CPU won!💻')
+elif computer == 5 and player == 3 :
+    print('The CPU won!💻')
+elif computer == 3 and player == 4 :
+    print('The CPU won!💻')
+elif computer == 4 and player == 2 :
+    print('The CPU won!💻')
+elif computer == 2 and player == 5 :
+    print('The CPU won!💻')
+elif computer == 5 and player == 1 :
+    print('The CPU won!💻')
+elif computer == 1 and player == 3 :
+    print('The CPU won!💻')
+elif player == 1 and computer == 1 :
+    print('Draw🥱')
+elif player == 2 and computer == 2 :
+    print('Draw🥱')
+elif player == 3 and computer == 3 :
+    print('Draw🥱')
+elif player == 4 and computer == 4 :
+    print('Draw🥱')
+elif player == 5 and computer == 5 :
+    print('Draw🥱')
+else:
+    print('Input error')
